579de34b5f1fd4d0e420e22753c464a92b3aec08
Herve Ngenzi Rutayisire <hrngenzi@gmail.com>
2025-11-17 00:22:44 +0200

    Removed testmonials

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import HeroSection from '../components/HeroSection';
 import FeaturesSection from '../components/FeaturesSection';
-import TestimonialsSection from '../components/TestimonialsSection';
 function Home() {
     return (
         <div className="min-h-screen bg-gray-50">
@@ -10,7 +9,6 @@ function Home() {
             <main>
                 <HeroSection />
                 <FeaturesSection />
-                <TestimonialsSection />
             </main>
             <Footer />
         </div>
